9a68875772eedef34816c27755dba6e853f4ca91
Hunter Lawson <hunterlaw9son@gmail.com>
2021-08-25 16:32:32 -0400

    add artwork area

---
+++ assets/js/sketches/test.js
@@ -0,0 +1,33 @@
+var div = document.getElementById('canvas-container');
+var x, y;
+var xSpeed, ySpeed;
+
+function setup() {
+    // for some reason the height is always around 100 pixels shorter?? add 100
+    var canvas = createCanvas(div.offsetWidth, div.offsetHeight + 100);
+    canvas.parent('canvas-container');
+
+    x = random(width);
+    y = random(height);
+    xSpeed = random(-10, 10);
+    ySpeed = random(-10, 10);
+}
+
+function draw() {
+    background(0);
+    x += xSpeed;
+    y += ySpeed;
+    if(x + 50 >= width || x - 50 <= 0) {
+        xSpeed *= -1;
+    }
+    if(y + 50 >= height || y - 50 <= 0) {
+        ySpeed *= -1;
+    }
+    ellipse(x, y, 100, 100);
+}
+
+function windowResized() {
+    div = document.getElementById('canvas-container');
+    resizeCanvas(div.offsetWidth, div.offsetHeight);
+    canvas.position(0, 0 ,'relative');
+}
